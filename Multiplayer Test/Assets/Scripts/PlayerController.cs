fd9648f9dfa71da7692c41eebb027144e0e9aea1
Ben Wannan <benwannan1@gmail.com>
2019-11-25 23:28:03 -0500

    🌟 Initial Commit

---
+++ Multiplayer Test/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Mirror
+{
+    public class PlayerController : NetworkBehaviour
+    {
+        [SyncVar]
+        Transform spriteTransform;
+
+        /*void OnDisable()
+        {
+            if (isLocalPlayer)
+            {
+                Camera.main.transform.SetParent(null);
+                Camera.main.transform.localPosition = new Vector3(0f, 50f, 0f);
+                Camera.main.transform.localEulerAngles = new Vector3(90f, 0f, 0f);
+            }
+        }*/
+
+        public override void OnStartLocalPlayer()
+        {
+            base.OnStartLocalPlayer();
+
+            spriteTransform = GetComponentInChildren<SpriteRenderer>().transform;
+
+            /*Camera.main.transform.SetParent(transform);
+            Camera.main.transform.localPosition = new Vector3(0f, 0f, -8f);
+            Camera.main.transform.localEulerAngles = new Vector3(0f, 0f, 0f);*/
+        }
+
+        [Header("Movement Settings")]
+        public float moveSpeed = 8f;
+
+        [Header("Diagnostics")]
+        public float horizontal = 0f;
+        public float vertical = 0f;
+
+        void Update()
+        {
+            if (!isLocalPlayer) return;
+
+            horizontal = Input.GetAxis("Horizontal");
+            vertical = Input.GetAxis("Vertical");
+        }
+
+        void FixedUpdate()
+        {
+            if (!isLocalPlayer) return;
+
+            float turnRate = 150f;
+            spriteTransform.Rotate(0f, 0f, -horizontal * Time.fixedDeltaTime * turnRate);
+
+            Vector2 direction = spriteTransform.up;
+            direction.Normalize();
+            direction *= moveSpeed * vertical * Time.fixedDeltaTime;
+
+            // actually move lol
+            transform.position += (Vector3)direction;
+        }
+    }
+}
+
---
+++ Multiplayer Test/Assets/Scripts/SpriteShadow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteShadow : MonoBehaviour
+{
+    public Vector2 offset = new Vector2(-0.2f, -0.2f);
+
+    private SpriteRenderer spriteCaster;
+    private SpriteRenderer spriteShadow;
+
+    private Transform transCaster;
+    private Transform transShadow;
+
+    public Material shadowMaterial;
+    public Color shadowColor;
+
+    void Start()
+    {
+        transCaster = transform;
+        transShadow = new GameObject().transform;
+        transShadow.parent = transCaster;
+        transShadow.gameObject.name = "Shadow";
+        transShadow.localRotation = Quaternion.identity;
+
+        spriteCaster = GetComponent<SpriteRenderer>();
+        spriteShadow = transShadow.gameObject.AddComponent<SpriteRenderer>();
+
+        spriteShadow.material = shadowMaterial;
+        spriteShadow.color = shadowColor;
+        spriteShadow.sortingLayerName = spriteCaster.sortingLayerName;
+        spriteShadow.sortingOrder = spriteCaster.sortingOrder - 1;
+    }
+
+    void LateUpdate()
+    {
+        transShadow.position = transform.parent.transform.position + (Vector3)offset;
+        spriteShadow.sprite = spriteCaster.sprite;
+    }
+}
